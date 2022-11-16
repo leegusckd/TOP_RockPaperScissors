f78f9e6c9de26f6a9dd6c8d0352eb9df117f1535
leegusckd <107225641+leegusckd@users.noreply.github.com>
2022-11-16 02:23:45 -0800

    Add files via upload

---
+++ rps_java.js
@@ -0,0 +1,10 @@
+//create a function "getComputerChoice" that will randomly select 
+//rock paper scissors
+
+    //maybe create an array ["rock", "paper", "scissors"], then 
+    //use the Math.random() function to create to have computer randomly choose
+
+
+
+
+
