207d86e20359ace53924a5ec606e89a48827c806
James <leegusckd7@gmail.com>
2022-11-16 23:54:34 -0800

    Created function getComputerChoice. This will have the computer randomly choose between rock, paper, scissors.

--- rps_java.js
+++ rps_java.js
@@ -4,7 +4,21 @@
     //maybe create an array ["rock", "paper", "scissors"], then 
     //use the Math.random() function to create to have computer randomly choose one
 
+const choices = ["Rock", "Paper", "Scissors"]
 
+    function getComputerChoice(choices) {
+            return choices[Math.floor(Math.random()*choices.length)];
+     }
+
+    console.log(getComputerChoice(choices))
+
+     //Another way to write this would be: 
+        //const getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
+        //console.log(getComputerChoice);
+
+
+
+     
 
 
 
